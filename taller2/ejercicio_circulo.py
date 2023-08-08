dd974f086c2f65f4dc4ed359b2e25b7758e576a1
Liana <limadio.uni@gmail.com>
2023-08-08 14:17:54 -0500

    ejercicio_circulo.py commit

--- taller2/ejercicio_circulo.py
+++ taller2/ejercicio_circulo.py
@@ -10,22 +10,22 @@ class Punto:
         return (self.x, self.y)
 class Circulo:
     def __init__(self, centro: Punto, radio: int):
-        self.centro: Punto = Punto(2,5)
-        self.radio: int = radio
+        self.centro = centro
+        self.radio = radio
     def area_circulo(self):
-        area = math.pi*(self.radio)**2
-        return area
+        return math.pi*(self.radio)**2
 
     def perimetro_circulo(self):
-        perimetro = 2*math.pi*self.radio
-        return perimetro
+        return 2*math.pi*self.radio
 
     def punto_circulo(self, x2: int, y2:int):
         #x**2+y**2=r**2
-        if x2**2 + y2**2 == self.radio**2:
-            print(f"El punto ({x2,y2}) pertenece al círculo")
+        if x2**2 + y2**2 <= self.radio**2:
+            print(f"El punto {x2,y2} pertenece al círculo")
         else:
-            print(f"El punto ({x2, y2}) NO pertenece al círculo")
+            print(f"El punto {x2, y2} NO pertenece al círculo")
 
 if __name__ == "__main__":
-    circulo1: Circulo= Circulo()
+    p_centro = Punto(0, 0)
+    circulo1 = Circulo(p_centro, 4)
+    circulo1.punto_circulo(3, 2)
